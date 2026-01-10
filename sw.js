bff089591687dcd40eba922910b9e93e7001b0e2
Yohann Zaoui <yohannzaoui@gmail.com>
2026-01-10 16:04:56 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-a
+const CACHE_NAME = 'hifi-player-v1';
+const assets = ['./', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
